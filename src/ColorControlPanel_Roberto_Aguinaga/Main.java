5bcd16e7df530dc4f20ece14cd843c6dc73d1a6b
Roberto Aguinaga <raguinaga1@outlook.com>
2020-06-22 15:00:32 -0500

    Added text area and text field to scene

--- src/ColorControlPanel_Roberto_Aguinaga/Main.java
+++ src/ColorControlPanel_Roberto_Aguinaga/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -14,19 +15,21 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     /**
-     *  start launches
+     *  start launches gui app
      * @param stage Stage reference
      */
     @Override
     public void start(Stage stage) {
-        // Create an array of sliders
-        // Element 0 represents Red values
-        // Element 1 represents Green values
-        // Element 2 represents Blue values
+        /* Create an array of sliders
+         * Element 0 represents Red values
+         * Element 1 represents Green values
+         * Element 2 represents Blue values
+         */
         Slider[] rgbSlideArray = new Slider[3];
 
         /* Create sliders with min values of 0, maxes of 255 and a
-         * starting value of zero. Also set
+         * starting value of zero. Also set tick units, and
+         * explicitly set orientation.
          */
         for (int i = 0; i < rgbSlideArray.length; i++) {
             rgbSlideArray[i] = new Slider(0, 255, 0);
@@ -46,16 +49,20 @@ public class Main extends Application {
         TextArea textBox = new TextArea();
         TextField userRGBValue = new TextField();
 
-        // Create an HBox
+        // Create HBoxes for sliders and their labels
         HBox redHbox = new HBox(10, redSliderLabel,
                 rgbSlideArray[0]);
         HBox greenHbox = new HBox(10, greenSliderLabel,
                 rgbSlideArray[1]);
         HBox blueHbox = new HBox(10, blueSliderLabel, rgbSlideArray[2]);
+
         // Create a VBox to house the Hboxes
-        VBox vbox = new VBox(2,redHbox,greenHbox,blueHbox);
-        // Create scene
+        VBox vbox = new VBox(2,redHbox,greenHbox,blueHbox,
+                userRGBValue,textBox);
+
+        // Create scene with Vbox as the root node.
         Scene scene = new Scene(vbox);
+
         // Set title and launch stage
         stage.setTitle("Color Demo");
         stage.setScene(scene);
