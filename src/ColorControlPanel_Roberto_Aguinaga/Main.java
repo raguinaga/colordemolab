b2fa80d016680e9f91e2de1e8477c84611c9d5b5
Roberto Aguinaga <raguinaga1@outlook.com>
2020-06-24 12:42:50 -0500

    Fixed yet another javafx import.

--- src/ColorControlPanel_Roberto_Aguinaga/Main.java
+++ src/ColorControlPanel_Roberto_Aguinaga/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
 import javafx.geometry.Orientation;
